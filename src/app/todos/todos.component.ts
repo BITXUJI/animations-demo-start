4732a91f40e7ac4772eaf47b882e520ee06ce742
bitxuji <bitxuji@gmail.com>
2023-11-20 23:44:15 +0900

    state

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./todos.component.css'],
   animations: [
     trigger('fade', [
+      state('void', style({ opacity: 0 })),
       transition('void => *', [
-        style({ opacity: 0 }),
         animate(2000)
       ]),
-      transition('* => void', animate(2000, style({ opacity: 0 })))
+      transition('* => void', [
+        animate(2000)
+      ])
     ])
   ]
 })
