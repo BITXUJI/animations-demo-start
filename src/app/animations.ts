5c8b9336c56af7bb262d7e7084315d919816e79a
bitxuji <bitxuji@gmail.com>
2023-11-21 20:59:24 +0900

    add keyframes and cubic-bezier

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 export let fade = trigger('fade', [
     state('void', style({ opacity: 0 })),
     transition(':enter,:leave', [
@@ -8,10 +8,20 @@ export let fade = trigger('fade', [
 export let slide = trigger('slide', [
     transition(':enter', [
         style({ transform: 'translateX(-10%' }),
-        animate('500ms ease-out')
+        animate('500ms cubic-bezier(.61,.29,.07,1.02)')
     ]),
     transition(':leave', [
-        animate('2000ms ease-out', style({ transform: 'translateX(100%' }))
+        animate('2000ms ease-out', keyframes([
+            style({
+                offset: .2,
+                opacity: 1,
+                transform: 'translateX(20px)'
+            }),
+            style({
+                offset: 1,
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
     ]),
-
 ]);
