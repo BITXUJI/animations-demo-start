c7eae7fc6bc34276c7793a82805a8865ad314325
bitxuji <bitxuji@gmail.com>
2023-11-21 20:22:06 +0900

    add slide animation to todos

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -5,3 +5,13 @@ export let fade = trigger('fade', [
         animate(2000)
     ])
 ]);
+export let slide = trigger('slide', [
+    transition(':enter', [
+        style({ transform: 'translateX(-10%' }),
+        animate('500ms ease-out')
+    ]),
+    transition(':leave', [
+        animate('2000ms ease-out', style({ transform: 'translateX(100%' }))
+    ]),
+
+]);
