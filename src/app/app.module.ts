5e41f351b63a4dfeb397cd364fbd2e6ea9df5da5
bitxuji <bitxuji@gmail.com>
2023-11-23 12:46:28 +0900

    generate zippy and intall fortawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodosComponent } from './todos/todos.component'
+import { TodosComponent } from './todos/todos.component';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
